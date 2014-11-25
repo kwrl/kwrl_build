050c8af84ad9905c1bd2c4bd904b39fa623b3004
Haakon Kaurel <haakongk@stud.ntnu.no>
2014-11-25 15:01:12 -0500

    Some minor fixes, due to pylint being mean to me

--- kwrl_build.py
+++ kwrl_build.py
@@ -1,6 +1,6 @@
 from io_utils import *
+from styles import note_style
 from print_utils import print_debug, print_error, print_info
-from styles import *
 from languages import Language
 from themes import Theme
 from projects import Project
@@ -24,28 +24,35 @@ except ImportError:
     sys.exit(-10)
 
 # todo : real classes
-Term = collections.namedtuple('Term', 'id manifest title description language number projects extras')
+Term = collections.namedtuple('Term', \
+    'id manifest title description language number projects extras')
 Extra = collections.namedtuple('Extra', 'name materials note')
 
 # Process files within project and resource containers
 def build_extra(term, extra, language, theme, output_dir):
     note = []
     if extra.note:
-        note.extend(process_file(extra.note, note_style, language, theme, output_dir))
+        note.extend(process_file(extra.note, \
+            note_style, \
+            language, \
+            theme, \
+            output_dir))
     materials = None
     if extra.materials:
-        zipfilename = "%s_%d_%s_%s.zip" % (term.id, term.number, extra.name, language.translate("resources"))
-        materials = zip_files(os.path.dirname(term.manifest), extra.materials,output_dir, zipfilename)
-    return Extra(name = extra.name, note=note, materials=materials)
+        zipfilename = "%s_%d_%s_%s.zip" % \
+            (term.id, term.number, extra.name, language.translate("resources"))
+        materials = zip_files(os.path.dirname(term.manifest), \
+            extra.materials, output_dir, zipfilename)
+    return Extra(name=extra.name, note=note, materials=materials)
 
 def sort_files(files):
     sort_key = {
         'html':2,
         'pdf':1,
     }
-    return sorted(files, key=lambda x:sort_key.get(x.format,0), reverse=True)
+    return sorted(files, key=lambda x: sort_key.get(x.format, 0), reverse=True)
 
-#Find manifests and add all build languages 
+#Find manifests and add all build languages
 def get_termlangs(repositories, all_languages):
     print_info("Searching for manifests ..")
     termlangs = {}
@@ -65,7 +72,7 @@ def get_termlangs(repositories, all_languages):
 # The all singing all dancing build function of doing everything.
 def build(repositories, theme, all_languages, output_dir):
     termlangs = get_termlangs(repositories, all_languages)
-    
+
     print_info("Copying assets")
     copydir(html_assets, output_dir)
     css_dir = os.path.join(output_dir, "css")
@@ -78,50 +85,46 @@ def build(repositories, theme, all_languages, output_dir):
     for language_code, terms in termlangs.items():
         if language_code not in all_languages:
             all_languages[language_code] = Language(
-                code = language_code,
-                name = language_code,
-                legal = {},
-                translations = {}
+                code=language_code,
+                name=language_code,
+                legal={},
+                translations={}
             )
         language = all_languages[language_code]
         print_info("Language "+language.name)
         out_terms = []
-        count = 0;
+        count = 0
         lang_dir = os.path.join(output_dir, language.code)
 
         for term in terms:
             term_dir = os.path.join(lang_dir, "%s.%d"%(term.id, term.number))
             makedirs(term_dir)
-            print_info("Building Term:" + str(term.title))
+            print_info("Building Term:\t\t" + str(term.title))
             projects = []
+
             for p in term.projects:
-                try: 
-                    print_info("Building project:\t" + str(p.filename))
-                except:
+                built_project = Project.build_from_resource(p, \
+                    term, \
+                    term_dir, \
+                    language, \
+                    theme)
+                if built_project:
+                    count += 1
+                    projects.append(built_project)
+                else:
                     continue
-                count+=1
-                project = parse_project_meta(p)
-                project_dir = os.path.join(term_dir,"%.02d"%(project.number))
-                makedirs(project_dir)
-                try:
-                    built_project = Project.build_project(term, project, language, theme, project_dir)
-                except:
-                    print_error("Failed project: " + str(p.filename))
-                    continue
-                projects.append(built_project)
-                print_info("Project done:\t\t" + str(p.filename))
 
             extras = []
             for r in term.extras:
                 extras.append(build_extra(term, r, language, theme, term_dir))
 
             term = Term(
-                id = term.id,
+                id=term.id,
                 manifest=term.manifest,
-                number = term.number, language = term.language,
-                title = term.title, description= term.description,
-                projects = projects,
-                extras = extras,
+                number=term.number, language=term.language,
+                title=term.title, description=term.description,
+                projects=projects,
+                extras=extras,
             )
 
             out_terms.append(make_term_index(term, language, theme, term_dir))
@@ -130,51 +133,56 @@ def build(repositories, theme, all_languages, output_dir):
 
         print_info("Building " + language.name +" index")
 
-        languages[language_code]=make_lang_index(language, out_terms, theme, lang_dir)
-        project_count[language_code]=count
+        languages[language_code] = \
+            make_lang_index(language, out_terms, theme, lang_dir)
+        project_count[language_code] = count
 
     print_info("Building " + theme.name + " index: " + output_dir)
 
-    sorted_languages =  []
-    for lang in sorted(project_count.keys(), key=lambda x:project_count[x], reverse=True):
+    sorted_languages = []
+    for lang in sorted(project_count.keys(), \
+            key=lambda x: project_count[x], reverse=True):
         sorted_languages.append((all_languages[lang], languages[lang]))
 
-    make_index(sorted_languages,all_languages[theme.language], theme, output_dir)
+    make_index(sorted_languages, \
+        all_languages[theme.language], \
+        theme, \
+        output_dir)
     print_info("Complete")
 
 def parse_manifest(filename):
-    fh = codecs.open(filename,"r","utf-8").read()
+    fh = codecs.open(filename, "r", "utf-8").read()
     json_manifest = json.loads(fh)
     base_dir = os.path.join(os.path.dirname(filename))
     projects = []
     for p in json_manifest['projects']:
         filename = expand_glob(base_dir, p['filename'], one_file=True)
-        materials = expand_glob(base_dir, p.get('materials',[]))
-        embeds = expand_glob(base_dir, p.get('embeds',[]))
+        materials = expand_glob(base_dir, p.get('materials', []))
+        embeds = expand_glob(base_dir, p.get('embeds', []))
 
         if 'note' in p:
             note = expand_glob(base_dir, p['note'], one_file=True)
         else:
             note = None
-    
+ 
         project = Project(
-            filename = filename,
-            number = p['number'],
-            title = p.get('title', None),
-            materials = materials,
-            note = note,
-            embeds = embeds,
+            filename=filename,
+            number=p['number'],
+            title=p.get('title', None),
+            materials=materials,
+            note=note,
+            embeds=embeds,
         )
         projects.append(project)
 
     extras = []
-    for s in json_manifest.get('extras',()):
+    for s in json_manifest.get('extras', ()):
         if 'note' in s:
             note = expand_glob(base_dir, s['note'], one_file=True)
         else:
             note = None
         materials = expand_glob(base_dir, s.get('materials', ()))
-        
+ 
         extras.append(Extra(
             name=s['name'],
             note=note,
@@ -182,72 +190,16 @@ def parse_manifest(filename):
         ))
 
     m = Term(
-        id = json_manifest['id'],
-        title = json_manifest['title'],
+        id=json_manifest['id'],
+        title=json_manifest['title'],
         manifest=filename,
-        description = json_manifest['description'],
-        language = json_manifest['language'],
-        number = int(json_manifest['number']),
-        projects = projects,
-        extras = extras,
+        description=json_manifest['description'],
+        language=json_manifest['language'],
+        number=int(json_manifest['number']),
+        projects=projects,
+        extras=extras,
     )
     return m
-
-def parse_project_meta(p):
-    if not p.filename.endswith('md'):
-        return p
-
-    with codecs.open(p.filename,"r", "utf-8") as fh:
-        in_header = False
-        header_lines = []
-        for line in fh.readlines():
-            l = line.strip()
-            if l == "---":
-                in_header = True
-            elif l == "...":
-                in_header = False
-            elif in_header:
-                header_lines.append(line)
-    header = yaml.safe_load("".join(header_lines))
-
-    if header:
-        title   = header.get('title', p.title)
-        number  = header.get('number', p.number)
-        title   = header.get('title', p.title)
-
-        raw_note = header.get('note', None)
-        if raw_note:
-            base_dir = os.path.dirname(p.filename)
-            note = expand_glob(base_dir, raw_note, one_file=True)
-        else:
-            note = p.note
-
-        raw_materials = header.get('materials', ())
-        if raw_materials:
-            base_dir = os.path.dirname(p.filename)
-            materials = expand_glob(base_dir, raw_materials)
-            materials.extend(p.materials)
-        else:
-            materials = p.materials
-
-        raw_embeds = header.get('embeds', ())
-        if raw_embeds:
-            base_dir = os.path.dirname(p.filename)
-            embeds = expand_glob(base_dir, raw_embeds)
-            embeds.extend(p.embeds)
-        else:
-            embeds = p.embeds
-
-        return Project(
-            filename = p.filename,
-            number = number,
-            title = title,
-            materials = materials,
-            note = note,
-            embeds = embeds,
-        )
-    else:
-        return p
 
 def make_css(stylesheet_dir, theme, output_dir):
     for asset in os.listdir(stylesheet_dir):
@@ -260,19 +212,20 @@ def make_css(stylesheet_dir, theme, output_dir):
                     makedirs(dst)
                 else:
                     os.remove(dst)
-                    
+
             if os.path.isdir(src):
                 make_css(src, theme, dst)
             else:
                 if asset.endswith('.css'):
-                    with codecs.open(src,"r", "utf-8") as src_fh, open(dst,"w") as dst_fh:
+                    with codecs.open(src,"r", "utf-8") as src_fh, \
+                            codecs.open(dst, "w", "utf-8") as dst_fh:
                         template = string.Template(src_fh.read())
                         dst_fh.write(template.substitute(theme.css_variables))
                 else:
                     shutil.copy(src, output_dir)
 
-THEMES      = Theme.load_themes(theme_base)
-LANGUAGES   = Language.load_languages(language_base)
+THEMES = Theme.load_themes(theme_base)
+LANGUAGES = Language.load_languages(language_base)
 
 if __name__ == '__main__':
     args = sys.argv[1::]
--- projects.py
+++ projects.py
@@ -1,7 +1,10 @@
-from io_utils import process_file, copy_file, zip_files
+from io_utils import process_file, copy_file, zip_files, makedirs, expand_glob
+from print_utils import print_info, print_error
 from styles import lesson_style_debug, note_style
 from html_builders import markdown_to_html
 import os.path
+import codecs
+import yaml
 
 class Project:
     def __init__(self, filename, number, title, materials, note, embeds):
@@ -11,6 +14,27 @@ class Project:
         self.materials  = materials
         self.note       = note
         self.embeds     = embeds
+
+    @staticmethod
+    def build_from_resource(resource, term, term_dir, language, theme):
+        try: 
+            print_info("Project building..\t" + str(resource.filename))
+        except:
+            print_error("Project failed due to filename encoding: " + str(resource.filename))
+            return None
+
+        project     = Project.parse_project_meta(resource)
+        project_dir = os.path.join(term_dir,"%.02d"%(project.number))
+        makedirs(project_dir)
+
+        try:
+            built_project = Project.build_project(term, project, language, theme, project_dir)
+        except:
+            print_error("Project failed while building: " + str(resource.filename))
+            return None
+
+        print_info("Project done!\t\t" + str(resource.filename))
+        return built_project
 
     @staticmethod
     def build_project(term, project, language, theme, output_dir):
@@ -45,4 +69,58 @@ class Project:
             embeds = embeds,
         )
 
+    def parse_project_meta(p):
+        if not p.filename.endswith('md'):
+            return p
 
+        with codecs.open(p.filename,"r", "utf-8") as fh:
+            in_header = False
+            header_lines = []
+            for line in fh.readlines():
+                l = line.strip()
+                if l == "---":
+                    in_header = True
+                elif l == "...":
+                    in_header = False
+                elif in_header:
+                    header_lines.append(line)
+        header = yaml.safe_load("".join(header_lines))
+
+        if header:
+            title   = header.get('title', p.title)
+            number  = header.get('number', p.number)
+            title   = header.get('title', p.title)
+
+            raw_note = header.get('note', None)
+            if raw_note:
+                base_dir = os.path.dirname(p.filename)
+                note = expand_glob(base_dir, raw_note, one_file=True)
+            else:
+                note = p.note
+
+            raw_materials = header.get('materials', ())
+            if raw_materials:
+                base_dir = os.path.dirname(p.filename)
+                materials = expand_glob(base_dir, raw_materials)
+                materials.extend(p.materials)
+            else:
+                materials = p.materials
+
+            raw_embeds = header.get('embeds', ())
+            if raw_embeds:
+                base_dir = os.path.dirname(p.filename)
+                embeds = expand_glob(base_dir, raw_embeds)
+                embeds.extend(p.embeds)
+            else:
+                embeds = p.embeds
+
+            return Project(
+                filename = p.filename,
+                number = number,
+                title = title,
+                materials = materials,
+                note = note,
+                embeds = embeds,
+            )
+        else:
+            return p
